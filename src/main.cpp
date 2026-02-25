90046035e862b33af2084c26fc75569f674b0104
Mark Trevor Birss <markbirss@gmail.com>
2026-02-25 11:54:18 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,17 @@
 #define EPD_BUSY   6
 #define EPD_SCK    8
 #define EPD_MOSI   10
+#define MESSAGE "Hello GxEPD2_583_GDEQ0583T31"
 
-// --- Display Selection (Example: 1.54" 200x200) ---
-// Find your specific constructor in GxEPD2_display_selection.h
-GxEPD2_BW<GxEPD2_426_GDEQ0426T82, GxEPD2_426_GDEQ0426T82::HEIGHT> display(GxEPD2_426_GDEQ0426T82(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+// --- Display Selection
+// Find your specific constructor in
+// https://github.com/ZinggJM/GxEPD2/blob/master/examples/GxEPD2_Example/GxEPD2_display_selection_new_style.h
+
+//GxEPD2_BW<GxEPD2_426_GDEQ0426T82, GxEPD2_426_GDEQ0426T82::HEIGHT> display(GxEPD2_426_GDEQ0426T82(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+
+GxEPD2_BW<GxEPD2_583_GDEQ0583T31, GxEPD2_583_GDEQ0583T31::HEIGHT> display(GxEPD2_583_GDEQ0583T31(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+
+//GxEPD2_BW<GxEPD2_290_T94, GxEPD2_290_T94::HEIGHT> display(GxEPD2_290_T94(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 
 void helloWorld() {
   display.setRotation(1);
@@ -22,7 +29,7 @@ void helloWorld() {
   display.setTextColor(GxEPD_BLACK);
   
   int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds("Hello C3", 0, 0, &tbx, &tby, &tbw, &tbh);
+  display.getTextBounds(MESSAGE, 0, 0, &tbx, &tby, &tbw, &tbh);
   uint16_t x = (display.width() - tbw) / 2;
   uint16_t y = (display.height() + tbh) / 2;
   
@@ -31,7 +38,7 @@ void helloWorld() {
   do {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(x, y);
-    display.print("Hello C3");
+    display.print(MESSAGE);
   } while (display.nextPage());
 }
 
